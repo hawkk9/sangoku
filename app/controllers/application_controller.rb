c4a9a8212cc842d2d52cb470b5718c43d924a5dc
hawkk9 <yuichi19930508@gmail.com>
2020-01-10 21:53:05 +0900

    fix undefined local variable or method `dummy_user_id'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def dummy_user_id
+    1
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def dummy_user_id
-    1
-  end
-
   def javascript_path
     "#{controller_path}/#{action_name}.js"
   end
