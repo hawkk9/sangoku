f53ec64dff7f8121cbf681a29ab306b072e8b3f6
hawkk9 <yuichi19930508@gmail.com>
2020-01-15 21:18:27 +0900

    メッセージログに色を付ける

--- app/models/conscription_command.rb
+++ app/models/conscription_command.rb
@@ -8,7 +8,7 @@ class ConscriptionCommand < ApplicationRecord
     user.soldier_type = self.soldier_type
     user.soldier_num += self.soldier_num
     user.save
-    ["●11月:#{user.soldier.name_with_rank}を+#{self.soldier_num}徴兵しました。金：-#{price}(13日19時57分)"]
+    ["<font color='navy'>●</font>11月:#{user.soldier.name_with_rank}を<font color='red'>+#{self.soldier_num}</font>徴兵しました。金：-<font color='#0000ff'>#{price}</font>(13日19時57分)"]
   end
 
   def inputed_label(user)
