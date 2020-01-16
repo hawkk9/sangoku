8b57eb5a226e9f1955392feabc6ce60fd76e9b18
hawkk9 <yuichi19930508@gmail.com>
2020-01-16 22:40:36 +0900

    徴兵コマンドの失敗判定

--- app/models/conscription_command.rb
+++ app/models/conscription_command.rb
@@ -20,20 +20,39 @@ class ConscriptionCommand < ApplicationRecord
   end
 
   def validate_can_employ
-    return message('【徴兵失敗】：お金が足りないので徴兵できませんでした。') if self.price > self.user.gold
+    return message('【徴兵失敗】：金が足りませんです。') if self.price > self.user.gold
+    return message('【徴兵失敗】：農民が居ないので徴兵できませんでした。') if self.town.farmer < self.need_farmer
+    return message('【徴兵失敗】：民忠が低いです。') if self.town.allegiance < self.need_allegiance
+    return message('【徴兵失敗】：この都市にはその兵種を雇う技術が無いようです。') if self.town.technology < self.soldier.technology
   end
 
   def inputed_label
-    "#{user.soldier.name_with_rank}#{Command::COMMAND_LABEL_HASH[self.command.command_type]}（#{self.soldier_num}人:#{self.price}Ｇ）"
+    "#{soldier.name_with_rank}#{Command::COMMAND_LABEL_HASH[self.command.command_type]}（#{self.soldier_num}人:#{self.price}Ｇ）"
   end
 
   protected
 
   def price
-    self.user.soldier.gold * self.soldier_num
+    self.soldier.gold * self.soldier_num
+  end
+
+  def need_farmer
+    self.soldier_num * 8
+  end
+
+  def need_allegiance
+    self.soldier_num / 10
   end
 
   def user
     self.command.user
+  end
+
+  def town
+    self.user.town
+  end
+
+  def soldier
+    self.user.soldier
   end
 end
