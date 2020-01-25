fa4377ebf6aaee90ea6e7aea18e807c7583fb209
hawkk9 <yuichi19930508@gmail.com>
2020-01-25 15:39:21 +0900

    スキル種別のenum定義

---
+++ app/models/concerns/battle/action_invoker.rb
@@ -0,0 +1,11 @@
+module Battle
+  class ActionInvoker
+    def initialize(probability)
+      @probability = probability
+    end
+
+    def invoke(attack_user, defence_user)
+
+    end
+  end
+end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,33 @@
 class Skill < ApplicationRecord
   belongs_to :user
+
+  enum skill_type: {
+    infantry: 0, cavalry: 1, archer: 2,
+    move: 3, strategy: 4, tactics: 5,
+    leader: 6, ninja: 7, plot: 8,
+    agitation: 9, training: 10, fight: 11,
+    anabasis: 12, besieging:13, bandit: 14,
+    virtue: 15, elite: 16,
+  }
+
+
+  SKILL_OPTIONS = [
+    {
+      type: :ninja,
+      level: 1,
+    }
+  ]
+
+  class << self
+    def filter_by_level(skills)
+
+    end
+
+    def options_by_skill(skill)
+      SKILL_OPTIONS.filter do |skill_option|
+        (skill_option[:type] == skill.skill_type.to_sym) &&
+          (skill_option[:level] <= skill.level)
+      end
+    end
+  end
 end
