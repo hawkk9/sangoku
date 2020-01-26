3d196116f599167b07f0f8a6ee7581679f1eec57
hawkk9 <yuichi19930508@gmail.com>
2020-01-26 20:20:58 +0900

    格闘

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -213,6 +213,67 @@ class Skill < ApplicationRecord
       battling: nil,
       timings: []
     },
+    {
+      type: :training,
+      level: 1,
+      battling: nil,
+      timings: []
+    },
+    {
+      type: :training,
+      level: 2,
+      battling: nil,
+      timings: []
+    },
+    {
+      type: :training,
+      level: 3,
+      battling: nil,
+      timings: []
+    },
+    {
+      type: :fight,
+      level: 1,
+      battling: Proc.new do |user, opponent_user|
+        odds = user.strength / 10
+        if rand(1..100) <= odds
+          min_damage = user.max_damage / 2
+          user.damage = [user.damage, min_damage].max
+          Message::MessageWriter.message(
+            "【会心の一撃】#{user.name}の会心の一撃！"
+          )
+        end
+      end,
+      timings: []
+    },
+    {
+      type: :fight,
+      level: 2,
+      battling: Proc.new do |user, opponent_user|
+        odds = user.strength / 11
+        if rand(1..100) <= odds
+          user.max_damage += rand(1..2)
+          Message::MessageWriter.message(
+            "【気合いため】#{user.name}の最大ダメージが上昇しました！(#{user.name}の最大ダメージ＝#{user.max_damage})"
+          )
+        end
+      end,
+      timings: []
+    },
+    {
+      type: :fight,
+      level: 3,
+      battling: Proc.new do |user, opponent_user|
+        odds = user.strength / 14
+        if rand(1..100) <= odds
+          user.damage = user.damage * 2
+          Message::MessageWriter.message(
+            "【正拳突き】#{user.name}の正拳突き！"
+          )
+        end
+      end,
+      timings: []
+    },
   ]
 
   class << self
