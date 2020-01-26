1ed20d713617cbc0dc547db2263f66a763ed2759
hawkk9 <yuichi19930508@gmail.com>
2020-01-26 12:43:23 +0900

    幻影、受け流し

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -135,6 +135,40 @@ class Skill < ApplicationRecord
       end,
       timings: []
     },
+    {
+      type: :ninja,
+      level: 1,
+      battling: Proc.new do |user, opponent_user|
+        odds = user.leadership / 11
+        if rand(1..100) <= odds
+          opponent_user.damage = 0
+          Message::MessageWriter.message(
+            "【幻影】#{user.name}が幻影を発動しました。#{user.name}は攻撃を受けません。"
+          )
+        end
+      end,
+      timings: []
+    },
+    {
+      type: :ninja,
+      level: 2,
+      battling: nil,
+      timings: []
+    },
+    {
+      type: :ninja,
+      level: 3,
+      battling: Proc.new do |user, opponent_user|
+        odds = user.leadership / 13
+        if rand(1..100) <= odds
+          user.damage += opponent_user.damage
+          Message::MessageWriter.message(
+            "【受け流し】#{user.name}が受け流しを発動しました。#{opponent_user.name}が与えるダメージが#{user.name}の攻撃力に加わります"
+          )
+        end
+      end,
+      timings: []
+    },
   ]
 
   class << self
