1486a03c8b3481827bd4b546548cee1c0ac69210
hawkk9 <yuichi19930508@gmail.com>
2020-03-09 23:37:31 +0900

    戦術

--- app/models/concerns/skills/senjutu_skill.rb
+++ app/models/concerns/skills/senjutu_skill.rb
@@ -1,85 +1,116 @@
 module Skills
   class SenjutuSkill < Skills::BaseSkill
-    BEFORE_BATTLE_EFFECTS = [
-      {
-        level: 1,
-        effect: Proc.new do |user, opponent_user|
-          enable = if user.strength >= user.intelligence
-                     user.strength > opponent_user.strength
-                   else
-                     user.intelligence > opponent_user.intelligence
-                   end
-          if enable
-            opponent_user.battle_param.add_status_percents(-0.05)
-            Message::MessageWriter.message(
-              "【不意打ち】不意打ちにより攻守が－５%になりました"
-            )
-          end
-        end,
-        conditions: [CONDITIONS[:attack]]
-      },
-      {
-        level: 2,
-        effect: Proc.new do |user, opponent_user|
+    class << self
+      def before_battle_effects
+        [
+          {
+            level: 1,
+            effect: method(:fuiuti_before_battle_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:attack]]
+          },
+          {
+            level: 2,
+            effect: method(:yasyu_before_battle_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:attack]]
+          },
+          {
+            level: 3,
+            effect: method(:kyousyu_before_battle_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:attack]]
+          },
+        ]
+      end
+
+      def battling_effects
+        [
+          {
+            level: 2,
+            effect: method(:yasyu_battling_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:attack]]
+          },
+          {
+            level: 3,
+            effect: method(:kyousyu_battling_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:attack]]
+          },
+        ]
+      end
+
+      def fuiuti_before_battle_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        enable = if user.strength >= user.intelligence
+                   user.strength > opponent_user.strength
+                 else
+                   user.intelligence > opponent_user.intelligence
+                 end
+        if enable
+          opponent_user.battle_param.add_status_percents(-0.05)
+          messages << Message::MessageWriter.message(
+            "【不意打ち】不意打ちにより攻守が－５%になりました"
+          )
+        end
+        messages
+      end
+
+      def yasyu_before_battle_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        if battle_context.mode == :yasyu.to_s
           enable = [user.strength, user.intelligence].max >= 130
           if enable
             opponent_user.battle_param.add_status_percents(-0.05)
-            Message::MessageWriter.message(
-              "【夜襲】夜襲により攻守が－５%になりました"
+            messages << Message::MessageWriter.message(
+              "【夜襲】夜襲により攻守が-5%になりました"
             )
           end
-        end,
-        conditions: [CONDITIONS[:attack]]
-      },
-      {
-        level: 3,
-        effect: Proc.new do |user, opponent_user|
+        end
+        messages
+      end
+
+      def kyousyu_before_battle_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        if battle_context.mode == :kyousyu.to_s
           enable = [user.strength, user.intelligence].max >= 160
           if enable
-            opponent_user.battle_param.add_status_percents(-0.05)
-            Message::MessageWriter.message(
-              "【強襲】強襲により攻守が－５%になりました"
+            opponent_user.battle_param.add_status_percents(-0.1)
+            messages << Message::MessageWriter.message(
+              "【強襲】強襲により攻守が-10%になりました"
             )
           end
-        end,
-        conditions: [CONDITIONS[:attack]]
-      }
-    ]
+        end
+        messages
+      end
 
-    BATTLING_EFFECTS = [
-      {
-        level: 2,
-        effect: Proc.new do |user, opponent_user|
-          if user.battle_param.mode == :ambush.to_s
-            odds = (user.strength + user.intelligence) / 10
-            if rand(1..100) <= odds
-              damage = rand(1..5)
-              opponent_user.soldier_num -= damage
-              Message::MessageWriter.message(
-                "【夜襲】#{user.name}が夜襲を仕掛けました。兵が数人倒されました。" \
+      def yasyu_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        if battle_context.mode == :yasyu.to_s
+          odds = (user.strength + user.intelligence) / 10
+          if Util::Calculator::draw_lots(odds)
+            damage = rand(1..5)
+            opponent_user.soldier_num -= damage
+            messages << Message::MessageWriter.message(
+              "【夜襲】#{user.name}が夜襲を仕掛けました。兵が数人倒されました。" \
               "#{opponent_user.name} #{opponent_user.soldier.name_with_rank} #{opponent_user.soldier_num}人 ↓(-#{damage})"
-              )
-            end
+            )
           end
-        end,
-        conditions: [CONDITIONS[:attack]]
-      },
-      {
-        level: 3,
-        effect: Proc.new do |user, opponent_user|
-          if user.battle_param.mode == :assault.to_s
-            odds = (user.intelligence + user.charm) / 7
-            if rand(1..100) <= odds
-              damage = rand(1..7)
-              opponent_user.soldier_num -= damage
-              Message::MessageWriter.message(
-                "【強襲】#{user.name}が強襲を仕掛けました。兵が数人倒されました。" \
+        end
+        messages
+      end
+
+      def kyousyu_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        if battle_context.mode == :kyousyu.to_s
+          odds = (user.intelligence + user.charm) / 7
+          if Util::Calculator::draw_lots(odds)
+            damage = rand(1..7)
+            opponent_user.soldier_num -= damage
+            messages << Message::MessageWriter.message(
+              "【強襲】#{user.name}が強襲を仕掛けました。兵が数人倒されました。" \
               "#{opponent_user.name} #{opponent_user.soldier.name_with_rank} #{opponent_user.soldier_num}人 ↓(-#{damage})"
-              )
-            end
+            )
           end
-        end,
-        conditions: [CONDITIONS[:attack]]
-      }]
+        end
+        messages
+      end
+    end
   end
 end
