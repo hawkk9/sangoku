6cd7b56d36dbb6aff5c36625a568e5bdc17bc0b9
hawkk9 <yuichi19930508@gmail.com>
2020-03-11 22:07:18 +0900

    格闘

--- app/models/concerns/skills/kakutou_skill.rb
+++ app/models/concerns/skills/kakutou_skill.rb
@@ -1,44 +1,62 @@
 module Skills
   class KakutouSkill < Skills::BaseSkill
-    BATTLING_EFFECTS = [{
-                 level: 1,
-                 effect: Proc.new do |user, opponent_user|
-                   odds = user.strength / 10
-                   if rand(1..100) <= odds
-                     min_damage = user.battle_param.max_damage / 2
-                     user.battle_param.damage = [user.battle_param.damage, min_damage].max
-                     Message::MessageWriter.message(
-                       "【会心の一撃】#{user.name}の会心の一撃！"
-                     )
-                   end
-                 end,
-                 conditions: []
-               },
-               {
-                 level: 2,
-                 effect: Proc.new do |user, opponent_user|
-                   odds = user.strength / 11
-                   if rand(1..100) <= odds
-                     user.battle_param.max_damage += rand(1..2)
-                     Message::MessageWriter.message(
-                       "【気合いため】#{user.name}の最大ダメージが上昇しました！(#{user.name}の最大ダメージ＝#{user.battle_param.max_damage})"
-                     )
-                   end
-                 end,
-                 conditions: []
-               },
-               {
-                 level: 3,
-                 effect: Proc.new do |user, opponent_user|
-                   odds = user.strength / 14
-                   if rand(1..100) <= odds
-                     user.battle_param.damage = user.battle_param.damage * 2
-                     Message::MessageWriter.message(
-                       "【正拳突き】#{user.name}の正拳突き！"
-                     )
-                   end
-                 end,
-                 conditions: []
-               }]
+    class << self
+      def battling_effects
+        [
+          {
+            level: 1,
+            effect: method(:kaisin_battling_effect),
+            conditions: []
+          },
+          {
+            level: 2,
+            effect: method(:kiai_battling_effect),
+            conditions: []
+          },
+          {
+            level: 3,
+            effect: method(:seiken_battling_effect),
+            conditions: []
+          }
+        ]
+      end
+
+      def kaisin_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        odds = user.strength / 10
+        if Util::Calculator::draw_lots(odds)
+          min_damage = user.battle_param.max_damage / 2
+          user.battle_param.damage = [user.battle_param.damage, min_damage].max
+          messages << Message::MessageWriter.message(
+            "【会心の一撃】#{user.name}の会心の一撃！"
+          )
+        end
+        messages
+      end
+
+      def kiai_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        odds = user.strength / 11
+        if Util::Calculator::draw_lots(odds)
+          user.battle_param.max_damage += rand(1..2)
+          messages << Message::MessageWriter.message(
+            "【気合いため】#{user.name}の最大ダメージが上昇しました！(#{user.name}の最大ダメージ＝#{user.battle_param.max_damage})"
+          )
+        end
+        messages
+      end
+
+      def seiken_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        odds = user.strength / 14
+        if Util::Calculator::draw_lots(odds)
+          user.battle_param.damage *= 2
+          messages << Message::MessageWriter.message(
+            "【正拳突き】#{user.name}の正拳突き！"
+          )
+        end
+        messages
+      end
+    end
   end
 end
