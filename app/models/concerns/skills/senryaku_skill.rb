73151a30f7ebeb08de19d8c3693b415368d32c80
hawkk9 <yuichi19930508@gmail.com>
2020-03-13 21:47:12 +0900

    戦略

--- app/models/concerns/skills/senryaku_skill.rb
+++ app/models/concerns/skills/senryaku_skill.rb
@@ -1,51 +1,71 @@
 module Skills
   class SenryakuSkill < Skills::BaseSkill
-    BATTLING_EFFECTS = [{
-                 level: 1,
-                 effect: Proc.new do |user, opponent_user|
-                   odds = (user.intelligence + user.charm) / 11
-                   if rand(1..100) <= odds
-                     damage = rand(1..7)
-                     opponent_user.soldier_num -= damage
-                     Message::MessageWriter.message(
-                       "【罠】#{user.name}が落とし穴を仕掛けました。" \
+    class << self
+      def battling_effects
+        [
+          {
+            level: 1,
+            effect: method(:otosiana_battling_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:defence]]
+          },
+          {
+            level: 2,
+            effect: method(:engun_battling_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:defence]]
+          },
+          {
+            level: 3,
+            effect: method(:mizuzeme_battling_effect),
+            conditions: [Skills::BaseSkill::CONDITIONS[:defence]]
+          },
+        ]
+      end
+
+      def otosiana_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        odds = (user.intelligence + user.charm) / 11
+        if Util::Calculator::draw_lots(odds)
+          damage = rand(1..7)
+          opponent_user.soldier_num -= damage
+          messages << Message::MessageWriter.message(
+            "【罠】#{user.name}が落とし穴を仕掛けました。" \
             "#{opponent_user.name} #{opponent_user.soldier.name_with_rank} #{opponent_user.soldier_num}人 ↓(-#{damage})"
-                     )
-                   end
-                 end,
-                 conditions: [CONDITIONS[:defence]]
-               },
-               {
-                 level: 2,
-                 effect: Proc.new do |user, opponent_user|
-                   odds = (user.intelligence + user.charm) / 14
-                   if rand(1..100) <= odds
-                     recovery = rand(1..5)
-                     user.battle_param.max_damage += 1
-                     user.soldier_num += recovery
-                     Message::MessageWriter.message(
-                       "【援軍】#{user.name}に援軍が到着しました。味方の攻撃力が上昇しました。" \
-            "#{user.name} (最大ダメージ＝#{user.battle_param.max_damage})#{opponent_user.soldier.name_with_rank} #{opponent_user.soldier_num}人 ↑(+#{recovery})" \
           )
-                   end
-                 end,
-                 conditions: [CONDITIONS[:defence]]
-               },
-               {
-                 level: 3,
-                 effect: Proc.new do |user, opponent_user|
-                   odds = (user.intelligence + user.charm) / 13
-                   if rand(1..100) <= odds
-                     damage = rand(1..5)
-                     opponent_user.soldier_num -= damage
-                     opponent_user.battle_param.max_damage -= 1
-                     Message::MessageWriter.message(
-                       "【罠】#{user.name}が水攻めを仕掛けました。敵軍の攻撃力が低下しました。" \
-           "#{opponent_user.name} (最大ダメージ＝#{opponent_user.battle_param.max_damage}) #{opponent_user.soldier.name_with_rank} #{opponent_user.soldier_num}人 ↓(-#{damage})"
-                     )
-                   end
-                 end,
-                 conditions: [CONDITIONS[:defence]]
-               }]
+        end
+        messages
+      end
+
+      def engun_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        odds = (user.intelligence + user.charm) / 14
+        if Util::Calculator::draw_lots(odds)
+          recovery = rand(1..5)
+          user.battle_param.max_damage += 1
+          user.soldier_num += recovery
+          messages << Message::MessageWriter.message(
+            "【援軍】#{user.name}に援軍が到着しました。味方の攻撃力が上昇しました。" \
+            "#{user.name} (最大ダメージ＝#{user.battle_param.max_damage})" \
+            "#{opponent_user.soldier.name_with_rank} #{user.soldier_num}人 ↑(+#{recovery})"
+          )
+        end
+        messages
+      end
+
+      def mizuzeme_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        odds = (user.intelligence + user.charm) / 13
+        if Util::Calculator::draw_lots(odds)
+          damage = rand(1..5)
+          opponent_user.soldier_num -= damage
+          opponent_user.battle_param.max_damage -= 1
+          messages << Message::MessageWriter.message(
+            "【罠】#{user.name}が水攻めを仕掛けました。敵軍の攻撃力が低下しました。" \
+            "#{opponent_user.name} (最大ダメージ＝#{opponent_user.battle_param.max_damage})" \
+            "#{opponent_user.soldier.name_with_rank} #{opponent_user.soldier_num}人 ↓(-#{damage})"
+          )
+        end
+        messages
+      end
+    end
   end
 end
