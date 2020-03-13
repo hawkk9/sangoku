80f2c95d1e273f85093eaf2cbfbdd7a0776430d3
hawkk9 <yuichi19930508@gmail.com>
2020-03-13 23:11:10 +0900

    能力強化

--- app/models/concerns/skills/noukyou_skill.rb
+++ app/models/concerns/skills/noukyou_skill.rb
@@ -1,4 +1,30 @@
 module Skills
   class NoukyouSkill < Skills::BaseSkill
+    class << self
+      def before_battle_effects
+        [
+          {
+            level: 1,
+            effect: method(:status_up_before_battle_effect),
+            conditions: []
+          },
+          {
+            level: 2,
+            effect: method(:status_up_before_battle_effect),
+            conditions: []
+          },
+          {
+            level: 3,
+            effect: method(:status_up_before_battle_effect),
+            conditions: []
+          },
+        ]
+      end
+
+      def status_up_before_battle_effect(user, opponent_user, battle_context, is_attack)
+        opponent_user.battle_param.add_status_percents(0.04)
+        []
+      end
+    end
   end
 end
