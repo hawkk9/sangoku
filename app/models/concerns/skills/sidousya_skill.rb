0817fe43e5176541df04508b353ff77454517ff1
hawkk9 <yuichi19930508@gmail.com>
2020-03-13 21:57:27 +0900

    指導者

--- app/models/concerns/skills/sidousya_skill.rb
+++ app/models/concerns/skills/sidousya_skill.rb
@@ -1,20 +1,31 @@
 module Skills
   class SidousyaSkill < Skills::BaseSkill
-    BATTLING_EFFECTS = [{
-                 level: 3,
-                 effect: Proc.new do |user, opponent_user|
-                   odds = (user.intelligence + user.charm) / 11
-                   if rand(1..100) <= odds
-                     damage = rand(1..5)
-                     opponent_user.soldier_num -= damage
-                     user.soldier_num += damage
-                     Message::MessageWriter.message(
-                       "【洗脳】#{user.name}が#{opponent_user.name}の兵を洗脳しました。" \
-            "#{opponent_user.name} #{opponent_user.soldier_num}人 ↓(-#{damage}) #{user.name} #{user.soldier_num}人 ↑(+#{damage})" \
+    class << self
+      def battling_effects
+        [
+          {
+            level: 3,
+            effect: method(:sennou_battling_effect),
+            conditions: []
+          }
+        ]
+      end
+
+      def sennou_battling_effect(user, opponent_user, battle_context, is_attack)
+        messages = []
+        odds = (user.intelligence + user.charm) / 11
+        if Util::Calculator::draw_lots(odds)
+          damage = rand(1..5)
+          opponent_user.soldier_num -= damage
+          user.soldier_num += damage
+          messages << Message::MessageWriter.message(
+            "【洗脳】#{user.name}が#{opponent_user.name}の兵を洗脳しました。" \
+            "#{opponent_user.name} #{opponent_user.soldier_num}人 ↓(-#{damage})" \
+            " #{user.name} #{user.soldier_num}人 ↑(+#{damage})"
           )
-                   end
-                 end,
-                 conditions: []
-               }]
+        end
+        messages
+      end
+    end
   end
 end
