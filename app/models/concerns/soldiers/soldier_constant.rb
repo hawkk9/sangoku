2f80a430042281495b4c01ed5473c182087f1501
hawkk9 <yuichi19930508@gmail.com>
2020-03-24 22:26:05 +0900

    soldierのenum用のハッシュを共通化

---
+++ app/models/concerns/soldiers/soldier_constant.rb
@@ -0,0 +1,17 @@
+module Soldiers
+  module SoldierConstant
+    SOLDIER_RANK = {
+      none: 0, none_sp: 1, c: 2, c_sp: 3,
+      b: 4, b_sp: 5, a: 6, a_sp: 7,
+      s: 8, siege: 9, monte: 10, macao: 11,
+      vegas: 12, mirror: 13
+    }
+
+    SOLDIER_TYPE = {
+      none: 0,
+      infantry: 1,
+      cavalry: 2,
+      archer: 3
+    }
+  end
+end
--- app/models/soldier.rb
+++ app/models/soldier.rb
@@ -1,17 +1,8 @@
 class Soldier < ApplicationRecord
-  enum rank: {
-    none: 0, none_sp: 1, c: 2, c_sp: 3,
-    b: 4, b_sp: 5, a: 6, a_sp: 7,
-    s: 8, siege: 9, monte: 10, macao: 11,
-    vegas: 12, mirror: 13
-  }, _prefix: true
+  include Soldiers::SoldierConstant
 
-  enum soldier_type: {
-    none: 0,
-    infantry: 1,
-    cavalry: 2,
-    archer: 3
-  }, _prefix: true
+  enum rank: SOLDIER_RANK, _prefix: true
+  enum soldier_type: SOLDIER_TYPE, _prefix: true
 
   belongs_to :user
 
