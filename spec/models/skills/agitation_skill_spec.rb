6708e0b1fb3b18781a3fafe732b707ae50f64876
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2021-04-15 22:14:39 +0900

    fix uninitialized constant Error

--- spec/models/skills/agitation_skill_spec.rb
+++ spec/models/skills/agitation_skill_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Skills::AgitationSkill, type: :model do
+RSpec.describe Skills::SendouSkill, type: :model do
   describe '#keiryaku_before_battle_effect' do
     let(:country) { create(:country) }
     let(:town) { create(:town, country: country) }
@@ -9,13 +9,13 @@ RSpec.describe Skills::AgitationSkill, type: :model do
     let(:battle_context) { Battle::BattleContext.new(nil, town) }
     context '攻撃側' do
       it 'ターン数が10増加すること' do
-        Skills::AgitationSkill::keiryaku_before_battle_effect(user, opponent_user, battle_context, true)
+        Skills::SendouSkill::keiryaku_before_battle_effect(user, opponent_user, battle_context, true)
         expect(battle_context.turn_limit).to eq 47
       end
     end
     context '守備側' do
       it 'ターン数が10減少すること' do
-        Skills::AgitationSkill::keiryaku_before_battle_effect(user, opponent_user, battle_context, false)
+        Skills::SendouSkill::keiryaku_before_battle_effect(user, opponent_user, battle_context, false)
         expect(battle_context.turn_limit).to eq 27
       end
     end
