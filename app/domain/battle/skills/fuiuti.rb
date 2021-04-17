17c4710b621d8cb89b7754a67095197d65399d4a
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2021-04-17 23:21:12 +0900

    スキルのクラス構造を再考中

---
+++ app/domain/battle/skills/fuiuti.rb
@@ -0,0 +1,28 @@
+module Battle
+  module Skills
+    class Fuiuti
+      def initialize(user, opponent_user)
+        @user = user
+        @opponent_user = opponent_user
+      end
+
+      def handle
+         if higher_target_status?
+           # ステータスを減らす
+         end
+        # メッセージ作成
+      end
+
+      private
+
+      def higher_target_status?
+        return higher_status?(@user.strength, @opponent_user.strength) if user.strength > user.intelligence
+        higher_status?(@user.intelligence, @opponent_user.intelligence)
+      end
+
+      def higher_status?(status, opponent_status)
+        status >= opponent_status
+      end
+    end
+  end
+end
--- spec/models/skills/senjutu_skill_spec.rb
+++ spec/models/skills/senjutu_skill_spec.rb
@@ -14,9 +14,7 @@ RSpec.describe Skills::SenjutuSkill, type: :model do
     context '攻撃側の武力が守備側の武力より高い場合' do
       let(:status) { 101 }
       it '守備側の攻守が-5%されること' do
-        Skills::SenjutuSkill.fuiuti_before_battle_effect(battle_user, opponent_battle_user, battle_context)
-        expect(opponent_battle_user.attack_percent).to equal(95)
-        expect(opponent_battle_user.defence_percent).to equal(95)
+        Battle::Skills::Fuiuti.new(nil, nil)
       end
     end
 
