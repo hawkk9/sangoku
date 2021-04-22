bc8c1dccb79ee414e727e7b881dcf7d68e314d51
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2021-04-22 22:50:27 +0900

    リファクタ

--- spec/domain/battle/skills/fuiuti_spec.rb
+++ spec/domain/battle/skills/fuiuti_spec.rb
@@ -3,22 +3,21 @@ require 'rails_helper'
 RSpec.describe Battle::Skills::Fuiuti, type: :model do
   describe '不意打ち' do
     let(:user) { build_stubbed(:user) }
-    let(:opponent_user) { build_stubbed(:user, strength: strength) }
+    let(:opponent_user) { build_stubbed(:user, strength: opponent_strength) }
     let(:before_battle_user) { Battle::BeforeBattleUser.new(user) }
     let(:before_battle_opponent_user) { Battle::BeforeBattleUser.new(opponent_user) }
-    let(:strength) { 100 }
+    let(:fuiuti) {Battle::Skills::Fuiuti.new(before_battle_user, before_battle_opponent_user)}
+    let(:opponent_strength) { 100 }
     context '攻撃側の武力が守備側の武力以上の場合' do
       it '守備側の攻守が-5%されること' do
-        fuiuti = Battle::Skills::Fuiuti.new(before_battle_user, before_battle_opponent_user)
         fuiuti.handle
         expect(before_battle_opponent_user.attack_percent).to equal(95)
         expect(before_battle_opponent_user.defence_percent).to equal(95)
       end
     end
     context '攻撃側の武力が守備側の武力より低いの場合' do
-      let(:strength) { 101 }
+      let(:opponent_strength) { 101 }
       it '守備側の攻守が-5%されないこと' do
-        fuiuti = Battle::Skills::Fuiuti.new(before_battle_user, before_battle_opponent_user)
         fuiuti.handle
         expect(before_battle_opponent_user.attack_percent).to equal(100)
         expect(before_battle_opponent_user.defence_percent).to equal(100)
