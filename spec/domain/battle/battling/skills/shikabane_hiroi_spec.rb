e8f335978301cb6c6651c0e5853a2255377d2e14
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2021-05-04 20:49:31 +0900

    不要なテストを削除

--- spec/domain/battle/battling/skills/shikabane_hiroi_spec.rb
+++ spec/domain/battle/battling/skills/shikabane_hiroi_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Battle::Battling::Skills::ShikabaneHiroi, type: :model do
-  describe '屍拾い' do
-    let(:user) { build_stubbed(:user, soldier: soldier) }
-    let(:opponent_strength) { 100 }
-    let(:opponent_user) { build_stubbed(:user, strength: opponent_strength) }
-    let(:battling_user) { Battle::Battling::DefenceUser.new(user) }
-    let(:battling_opponent_user) { Battle::Battling::AttackUser.new(opponent_user) }
-    let(:shikabane_hiroi) { Battle::Battling::Skills::ShikabaneHiroi.new(before_battle_user, before_battle_opponent_user) }
-    context '' do
-      it '守備側の攻守が-5%されること' do
-        fuiuti.handle
-        expect(before_battle_opponent_user.attack_percent).to equal(95)
-        expect(before_battle_opponent_user.defence_percent).to equal(95)
+  pending do
+    describe '屍拾い' do
+      let(:user) { build_stubbed(:user, soldier: soldier) }
+      let(:opponent_strength) { 100 }
+      let(:opponent_user) { build_stubbed(:user, strength: opponent_strength) }
+      let(:battling_user) { Battle::Battling::DefenceUser.new(user) }
+      let(:battling_opponent_user) { Battle::Battling::AttackUser.new(opponent_user) }
+      let(:shikabane_hiroi) { Battle::Battling::Skills::ShikabaneHiroi.new(before_battle_user, before_battle_opponent_user) }
+      context '' do
+        it '守備側の攻守が-5%されること' do
+          fuiuti.handle
+          expect(before_battle_opponent_user.attack_percent).to equal(95)
+          expect(before_battle_opponent_user.defence_percent).to equal(95)
+        end
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:town) { create(:town) }
-  let(:user) { create(:user) }
+  pending do
+    let!(:town) { create(:town) }
+    let(:user) { create(:user) }
 
-  describe 'ステータス' do
-    it '攻撃力が正しく計算されること' do
-      expect(user.attack).to eq(97.5)
-    end
-    it '攻撃力が正しく計算されること' do
-      expect(user.defence).to eq(98.5)
+    describe 'ステータス' do
+      it '攻撃力が正しく計算されること' do
+        expect(user.attack).to eq(97.5)
+      end
+      it '攻撃力が正しく計算されること' do
+        expect(user.defence).to eq(98.5)
+      end
     end
   end
 end
